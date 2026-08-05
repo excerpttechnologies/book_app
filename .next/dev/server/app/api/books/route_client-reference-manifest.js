01b76140743d7707ad9c8df696c6e3cc636001c7
Excerpt Tech <myexcerptech@gmail.com>
2026-08-05 10:54:44 +0530

    ajdja

--- .next/dev/server/app/api/books/route_client-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/api/books/route"]={"moduleLoading":{"prefix":"/_next/"},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"C:\\Users\\Admin\\Downloads\\bookstore---\\bookstore---\\src\\app\\api\\books\\route":[]},"rscModuleMapping":{},"edgeRscModuleMapping":{}};
--- .next/dev/server/app/api/testimonials/route_client-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/api/testimonials/route"]={"moduleLoading":{"prefix":"/_next/"},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"C:\\Users\\Admin\\Downloads\\bookstore---\\bookstore---\\src\\app\\api\\testimonials\\route":[]},"rscModuleMapping":{},"edgeRscModuleMapping":{}};
